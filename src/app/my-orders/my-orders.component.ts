47f020c6af24d85dd80af3e8c419e8e05c034571
Santiago <santiago.g.grossi@gmail.com>
2022-06-09 20:21:53 -0300

    filter orders by categories working

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../auth.service';
 import { Order } from '../models/order';
 import { OrderService } from '../services/order.service';
 import { Category } from '../models/category';
+import { map } from 'rxjs/operator/map';
 
 @Component({
   selector: 'app-my-orders',
@@ -34,7 +35,8 @@ export class MyOrdersComponent implements OnInit, OnDestroy {
         .subscribe(orders =>{
           this.categories = [];
 
-          this.orders = orders;
+          this.orders = orders.filter(o => o.userId === this.userId);;
+          console.log(this.orders)
           this.filteredOrders = 
             this.orders.filter(o => o.userId === this.userId);
             
@@ -59,7 +61,10 @@ export class MyOrdersComponent implements OnInit, OnDestroy {
 
               
 
-           
+           orders.map(or =>{
+             or.costo = "asd",
+             or.costo2 = "qwe"
+           });
 
         })
 
@@ -76,33 +81,36 @@ export class MyOrdersComponent implements OnInit, OnDestroy {
   }
   clearFilters(){
     this.filteredOrders = this.orders.filter(o => o.userId === this.userId);
+      this.categories.forEach(category => {
+      category.checked = false;
+      });
   }
 
   filterByCategory(category,event){
+
     let categoryFounded = this.categories.find(actual=>actual.name==category);
       categoryFounded.checked = !categoryFounded.checked;
-      if(event.target.checked == true){
-          this.filteredOrders= this.orders.filter( order => order.items.some( items => items.category.includes( category ) ) && order.userId == this.userId);
-      }else{
-        this.filteredOrders = this.orders.filter(o => o.userId === this.userId);
 
-      }
+      let atLeastOneCategoryChecked = this.categories.some(category => category.checked == true)
+      this.filterOrders(atLeastOneCategoryChecked);
+  
 
 
     }
 
-    filterOrders(category){
-      for (let i=0; i<this.categories.length; i++){
-        if(this.categories[i].checked){
-          this.filteredOrders= this.orders.filter( order => order.userId == this.userId && order.items.some( items => items.category.includes( this.categories[i].categoryname ) ) );
-
-        }
+    filterOrders(atLeastOneCategoryChecked){
+      if(atLeastOneCategoryChecked){
+        this.filteredOrders = this.orders.filter((order) => {
+          return order.items.some((item) => {
+            let category= this.categories.find(category => category.name == item.category && category.checked == true);
+            if(category)  return (item.category === category.name);
+            return false;
+          });
+        });
+      }else{
+        this.clearFilters();
       }
-      this.categories.forEach( function(category,index,array) {
-       // this.filteredOrders= this.orders.filter( order => order.items.some( items => items.category.includes( category ) ) && order.userId == this.userId);
-      })
-      
-
+         
     }
 
     changeShippedFilter(){
